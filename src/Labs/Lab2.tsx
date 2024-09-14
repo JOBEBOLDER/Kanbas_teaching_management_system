af83a42143d0a6b7becc63ff87b082f57145bf5d
jieyaochen <chen.jieyao@northeastern.edu>
2024-09-14 15:37:44 -0400

    a2 completed!

---
+++ src/Labs/Lab2.tsx
@@ -0,0 +1,12 @@
+import Lab1 from "./Lab1";
+
+export default function Lab2() {
+    return (
+        <div>
+            <h1>Labs</h1>
+            <Lab1 />
+            <h2>Lab 1</h2>
+            <h2>Lab 3</h2>
+        </div>
+    )
+}
---
+++ src/Labs/Lab3/index.tsx
@@ -0,0 +1,8 @@
+// src/Labs/Lab3/index.tsx
+export default function Lab3() {
+    return (
+      <div>
+        <h2>Lab 3</h2>
+      </div>
+    );
+  }
